d94b9c4980ff7ad0d85a6523d5a6beb90ca08048
SAJID2010994 <kamrul6666600000@gmail.com>
2026-03-10 17:15:00 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from my server!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
